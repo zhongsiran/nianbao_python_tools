1bc35dde1121ab428216789119f7d689bd7181de
zhongsiran <zhongsiran@gmail.com>
2019-06-06 17:28:14 +0800

    表头DONE

--- 导出数据库记录.py
+++ 导出数据库记录.py
@@ -2,18 +2,17 @@ import pymysql
 from openpyxl import load_workbook
 from openpyxl import Workbook
 from string import Template
-import os
-import time
+from datetime import date
 
 class ExportMysqlToXlsx:
     def __init__(self):
-        self.conn = self.get_conn()
-        self.cur = self.conn.cursor()
+        # self.conn = self.get_conn()
+        # self.cur = self.conn.cursor()
         self.workbook = Workbook()
         self.worksheet = self.workbook.active
         self.worksheet_headrow = ['注册号','名称','地址','登记电话','法定代表人','联络员','联络员电话']
-        self.worksheet_headrow.append(['历史电话记录','本年度电话记录','跟进情况'])
-        self.worksheet_headrow.append(['年报状态','电话情况','联系员电话情况'])
+        self.worksheet_headrow.extend(['历史电话记录','本年度电话记录','跟进情况'])
+        self.worksheet_headrow.extend(['年报状态','电话情况','联系员电话情况'])
         self.db_table_name = '2019_nianbao_corp'
         self.results = tuple()
         self.row = 1
@@ -44,17 +43,17 @@ class ExportMysqlToXlsx:
     def save_to_xlsx(self):
         if self.worksheet_headrow:
             for i, j in enumerate(self.worksheet_headrow):
-                self.worksheet.cell(row=1, column=j+1, value=j)
+                self.worksheet.cell(row=1, column=i+1, value=j)
         for result in self.results:
             print(result)
             for column in result:
                 print(column)
-        self.save_workboos()
+        self.save_workbook()
 
     def save_workbook(self):
         now = date.today()
         today = "%d-%d-%d" %(now.year, now.month, now.day)
-        self.workbook.save(today + '导出记录表')
+        self.workbook.save(today + '导出记录表.xlsx')
 
     def close_cur(self):
         self.cur.close()
